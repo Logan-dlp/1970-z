fca887f45f5f82f0832b630c8fadecb064ca6f1b
Logan <pix.59700@gmail.com>
2023-04-22 23:48:45 +0200

    Update Scriptable Objects ✅

--- PFA/Assets/_Core/Scripts/ScriptableObjects/Armes/ArmesData.cs
+++ PFA/Assets/_Core/Scripts/ScriptableObjects/Armes/ArmesData.cs
@@ -5,5 +5,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ArmeDataFiles", menuName = "Scriptable Objects/ArmeDataFiles")]
 public class ArmesData : ScriptableObject
 {
+    public string Name;
     
+    public GameObject Skin;
+    public ParticleSystem ParticuleShoot;
+    
+    public float BulletSpeed;
+    public float RealoadTime;
+    
+    /// <summary>
+    /// Au bout de combien de balles l'arme se recharge automatiquement.
+    /// </summary>
+    public int Reaload;
+
+    public AudioSource SoundShoot;
 }
--- PFA/Assets/_Core/Scripts/ScriptableObjects/Zombies/ZombiesData.cs
+++ PFA/Assets/_Core/Scripts/ScriptableObjects/Zombies/ZombiesData.cs
@@ -5,5 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ZombieDataFiles", menuName = "Scriptable Objects/ZombieDataFiles")]
 public class ZombiesData : ScriptableObject
 {
+    public string Name;
+
+    public GameObject Skin;
     
+    /*
+     * à modifier si il a plusieurs animation untiliser directement un tableau
+     *            voir si c'est bien "Animator" à utiliser
+     */
+    public Animator Animation;
+
+    public float Life;
+    public float Degat;
+    public float Speed;
+
+    public AudioSource SoundZombie;
 }
