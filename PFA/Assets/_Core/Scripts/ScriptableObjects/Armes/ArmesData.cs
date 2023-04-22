39a05d3755b54483cf463a7e6b862f85c0cffea4
Logan <pix.59700@gmail.com>
2023-04-22 21:15:18 +0200

    Update Scriptable Objects 🚧
    Create Scriptables files

---
+++ PFA/Assets/_Core/Scripts/ScriptableObjects/Armes/ArmesData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ArmeDataFiles", menuName = "Scriptable Objects/ArmeDataFiles")]
+public class ArmesData : ScriptableObject
+{
+    
+}
---
+++ PFA/Assets/_Core/Scripts/ScriptableObjects/Zombies/ZombiesData.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ZombieDataFiles", menuName = "Scriptable Objects/ZombieDataFiles")]
+public class ZombiesData : ScriptableObject
+{
+    
+}
