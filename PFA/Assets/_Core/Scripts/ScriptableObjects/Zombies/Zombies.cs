45d44253f58234fbee102686ed61c832d6930e48
Logan <pix.59700@gmail.com>
2023-04-24 13:02:23 +0200

    Update Zombies 🚧
    Set base of AI

--- PFA/Assets/_Core/Scripts/ScriptableObjects/Zombies/Zombies.cs
+++ PFA/Assets/_Core/Scripts/ScriptableObjects/Zombies/Zombies.cs
@@ -3,14 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class Zombies : MonoBehaviour
 {
+    public GameManager GameManager;
     public ZombiesData DataSources;
+    
+    private NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.baseOffset = 1;
+        GameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
 
     private void Start()
     {
         Instantiate(DataSources.Skin, transform);
+        GameManager.Debug();
     }
     /*
      * Trouver le joueur le plus proche (en boucle)
