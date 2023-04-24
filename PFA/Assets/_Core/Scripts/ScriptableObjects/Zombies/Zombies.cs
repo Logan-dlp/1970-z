4db1c6342a13eac135755fbd8ff43727aa79e651
Logan <pix.59700@gmail.com>
2023-04-24 13:54:10 +0200

    Update Zombie 🚧
    Détecte le Player le plus proche

--- PFA/Assets/_Core/Scripts/ScriptableObjects/Zombies/Zombies.cs
+++ PFA/Assets/_Core/Scripts/ScriptableObjects/Zombies/Zombies.cs
@@ -8,26 +8,48 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class Zombies : MonoBehaviour
 {
-    public GameManager GameManager;
-    public ZombiesData DataSources;
+    private GameManager gameManager;
     
+    public ZombiesData DataSources;
+
+    private float posPlusProche;
+    private GameObject Player;
     private NavMeshAgent agent;
 
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.baseOffset = 1;
-        GameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
     }
 
     private void Start()
     {
         Instantiate(DataSources.Skin, transform);
-        GameManager.Debug();
+        gameManager.Debug();
     }
+
+    private void Update()
+    {
+        LePlusProche();
+    }
+
     /*
-     * Trouver le joueur le plus proche (en boucle)
+     * Trouver le joueur le plus proche (en boucle) ✅
      * le suivre
      * Arrivé a une certaine distance Infliger des dégats à la personne suivit (et faire une autre animation que de marcher)
      */
+    void LePlusProche()
+    {
+        foreach (GameObject _player in gameManager.Player)
+        {
+            float _posP = Mathf.Abs(_player.transform.position.x + _player.transform.position.y);
+            float _pos = Mathf.Abs(transform.position.x + transform.position.y);
+            if (posPlusProche == null || _pos - _posP < posPlusProche)
+            {
+                posPlusProche = _pos + _posP;
+                Player = _player;
+            }
+        }
+    }
 }
