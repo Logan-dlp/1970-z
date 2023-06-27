e5502d264ac3aabefd416dbc9c2663ab3acb430e
Logan <pix.59700@gmail.com>
2023-06-27 13:29:03 +0200

    Lever glowing & navmesh

--- PFA/Assets/_Core/Scripts/Interaction/Items/Door/LeverInteraction.cs
+++ PFA/Assets/_Core/Scripts/Interaction/Items/Door/LeverInteraction.cs
@@ -3,8 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ItemsSettings))]
 public class LeverInteraction : MonoBehaviour, IInteractable
 {
+    private Material material;
     private Animator animator;
     private bool interactable = true;
     private GameManager gameManager;
@@ -12,9 +14,11 @@ public class LeverInteraction : MonoBehaviour, IInteractable
 
     private void Start()
     {
+        material = GetComponentInChildren<Material>();
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         animator.GetComponent<Animator>();
         price = GetComponent<ItemsSettings>();
+        material.SetFloat("_Power", 2);
     }
 
     public void Interact(GameObject _player)
@@ -24,6 +28,7 @@ public class LeverInteraction : MonoBehaviour, IInteractable
             animator.SetBool("On", true);
             gameManager.Nblever++;
             interactable = false;
+            material.SetFloat("_Power", 100);
             price.Price = 0;
         }
     }
